3acef459df83648fe049ff368cd38130ef39d4e6
for <nithins674@gmail.com>
2024-01-10 15:29:01 +0000

    started tcp ip server work

---
+++ main.cpp
@@ -0,0 +1,18 @@
+// main.cpp : Nithin
+// Desc : the starting point of a chat server app
+
+#include <iostream>
+#include <ws2tcpip.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+using namespace std;
+
+int main()
+{
+
+    // initialise winsock
+
+    // shut down winsock
+    return 0;
+}
